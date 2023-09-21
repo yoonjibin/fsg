47d22b37e5f7f1eea502cc159dda8b753cff28fa
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-09-21 21:10:38 +0900

    add orderRepository testCode

--- src/test/kotlin/team/iwfsg/fsg/domain/order/repository/OrderRepositoryTest.kt
+++ src/test/kotlin/team/iwfsg/fsg/domain/order/repository/OrderRepositoryTest.kt
@@ -1,18 +1,19 @@
 package team.iwfsg.fsg.domain.order.repository
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldNotBe
 import io.mockk.mockk
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import team.iwfsg.fsg.domain.order.persistence.repository.OrderRepository
 
 @DataJpaTest
 class OrderRepositoryTest : BehaviorSpec({
     Given("orderRepository") {
-        val orderRepository = mockk<OrderRepository>() {
+        val orderRepository = mockk<OrderRepository>()
            When("Create OrderRepository") {
                Then("OrderRepository is not null") {
                    orderRepository shouldNotBe null
                }
            }
-        }
     }
 })
