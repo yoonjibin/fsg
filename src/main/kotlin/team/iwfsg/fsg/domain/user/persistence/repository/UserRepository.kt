3230cba0376a53837ffa8d6e9c9380299f880962
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-09-21 13:54:41 +0900

    add user entity

--- src/main/kotlin/team/iwfsg/fsg/domain/user/persistence/repository/UserRepository.kt
+++ src/main/kotlin/team/iwfsg/fsg/domain/user/persistence/repository/UserRepository.kt
@@ -1,6 +1,7 @@
 package team.iwfsg.fsg.domain.user.persistence.repository
 
 import org.springframework.data.repository.CrudRepository
+import team.iwfsg.fsg.domain.user.persistence.entity.User
 
 interface UserRepository : CrudRepository<User, Long> {
 }
