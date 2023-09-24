6b78a07d62197a80072db09ff6a5b2f61fd1488c
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-09-25 08:12:15 +0900

    add order fk mapping

--- src/main/kotlin/team/iwfsg/fsg/domain/order/persistence/entity/Order.kt
+++ src/main/kotlin/team/iwfsg/fsg/domain/order/persistence/entity/Order.kt
@@ -1,6 +1,10 @@
 package team.iwfsg.fsg.domain.order.persistence.entity
 
 import jakarta.persistence.*
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import team.iwfsg.fsg.domain.product.persistence.entity.Product
+import team.iwfsg.fsg.domain.user.persistence.entity.User
 import java.time.LocalDateTime
 
 @Entity
@@ -9,6 +13,16 @@ class Order(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user", nullable = false)
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "product", nullable = false)
+    val product: Product,
 
     @Column(name = "created_at")
     val createdAt: LocalDateTime,
