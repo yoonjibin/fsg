b06eed159317e3a67a7374d75263a6531bb8e1be
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-09-25 11:13:52 +0900

    update jpaRepository naming

--- src/main/kotlin/team/iwfsg/fsg/domain/order/persistence/repository/OrdersRepository.kt
+++ src/main/kotlin/team/iwfsg/fsg/domain/order/persistence/repository/OrdersRepository.kt
@@ -3,5 +3,5 @@ package team.iwfsg.fsg.domain.order.persistence.repository
 import org.springframework.data.repository.CrudRepository
 import team.iwfsg.fsg.domain.order.persistence.entity.OrdersJpaEntity
 
-interface OrderRepository : CrudRepository<OrdersJpaEntity, Long> {
+interface OrdersRepository : CrudRepository<OrdersJpaEntity, Long> {
 }
