36f6162eed7e57e1290d385fe2a9e45cea43d2fc
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-09-30 18:05:30 +0900

    add exception handler

--- src/main/kotlin/team/iwfsg/fsg/global/error/exceptions/FsgException.kt
+++ src/main/kotlin/team/iwfsg/fsg/global/error/exceptions/FsgException.kt
@@ -2,4 +2,4 @@ package team.iwfsg.fsg.global.error.exceptions
 
 import team.iwfsg.fsg.global.error.ErrorCode
 
-open class FsgExceptions(val errorCode: ErrorCode) : RuntimeException(errorCode.message)
+open class FsgException(val errorCode: ErrorCode) : RuntimeException(errorCode.message)
