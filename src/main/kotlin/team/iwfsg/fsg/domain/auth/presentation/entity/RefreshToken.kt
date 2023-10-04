9fbbb898f134ce8e5ccc3a0472f6a478ec8503ea
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-10-04 20:22:57 +0900

    add refreshToken entity

---
+++ src/main/kotlin/team/iwfsg/fsg/domain/auth/presentation/entity/RefreshToken.kt
@@ -0,0 +1,17 @@
+package team.iwfsg.fsg.domain.auth.presentation.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+import java.util.concurrent.TimeUnit
+
+@RedisHash(value = "refresh_token")
+data class RefreshToken(
+    @Id
+    val userIdx: Long,
+    @Indexed
+    val token: String,
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    val expiredAt: Int
+)
