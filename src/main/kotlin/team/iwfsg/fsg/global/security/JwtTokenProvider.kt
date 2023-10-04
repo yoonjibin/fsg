dad16c5cbce1703beea74f0184f8d394ffe1abf4
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-10-04 17:10:03 +0900

    add jwt token provider

---
+++ src/main/kotlin/team/iwfsg/fsg/global/security/JwtTokenProvider.kt
@@ -0,0 +1,44 @@
+package team.iwfsg.fsg.global.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import java.security.Key
+import java.time.LocalDateTime
+import java.util.*
+
+class JwtTokenProvider(
+        private val securityProperties: SecurityProperties
+) {
+    fun generate(userId: Long): TokenResponse {
+        val accessToken = generateAccessToken(userId, securityProperties.accessSecret)
+        val refreshToken = generateRefreshToken(userId, securityProperties.refreshSecret)
+        val accessExpiredAt = getAccessTokenExpiredAt()
+        val refreshExpiredAt = getRefreshTokenExpiredAt()
+        // TODO 리프레쉬 토큰 저장 로직 구현 - 윤지빈
+        return TokenResponse(accessToken, refreshToken, accessExpiredAt, refreshExpiredAt)
+    }
+
+    private fun generateAccessToken(userId: Long, secret: Key): String =
+            Jwts.builder()
+                    .signWith(secret, SignatureAlgorithm.HS256)
+                    .setSubject(userId.toString())
+                    .claim("type", SecurityProperties.accessType)
+                    .setIssuedAt(Date())
+                    .setExpiration(Date(System.currentTimeMillis() + securityProperties.accessExp * 1000))
+                    .compact()
+
+    private fun generateRefreshToken(userId: Long, secret: Key): String =
+            Jwts.builder()
+                    .signWith(secret, SignatureAlgorithm.HS256)
+                    .setSubject(userId.toString())
+                    .claim("type", SecurityProperties.refreshType)
+                    .setIssuedAt(Date())
+                    .setExpiration(Date(System.currentTimeMillis() + securityProperties.accessExp * 1000))
+                    .compact()
+
+    private fun getAccessTokenExpiredAt(): LocalDateTime =
+            LocalDateTime.now().plusSeconds(securityProperties.accessExp.toLong())
+
+    private fun getRefreshTokenExpiredAt(): LocalDateTime =
+            LocalDateTime.now().plusSeconds(securityProperties.refreshExp.toLong())
+}
