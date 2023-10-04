9284f6e5dbdce785489770f4271d497bcdbc0a52
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-10-04 17:25:09 +0900

    add error code

--- src/main/kotlin/team/iwfsg/fsg/global/error/ErrorCode.kt
+++ src/main/kotlin/team/iwfsg/fsg/global/error/ErrorCode.kt
@@ -1,6 +1,8 @@
 package team.iwfsg.fsg.global.error
 
 enum class ErrorCode(val message: String, val status: Int) {
+    EXPIRED_TOKEN("expired token", 401),
+    UNAUTHORIZED("unauthorized",401),
     FORBIDDEN("forbidden", 403),
     USER_NOT_FOUND("user not found", 404),
     INTERVAL_SERVER_ERROR("internal server error", 500);
