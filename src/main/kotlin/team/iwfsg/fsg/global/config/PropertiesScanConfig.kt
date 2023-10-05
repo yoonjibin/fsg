07a55d153875a699d20b84b4aad2b2ffbef09b5a
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-10-05 10:22:07 +0900

    add redis properties

--- src/main/kotlin/team/iwfsg/fsg/global/config/PropertiesScanConfig.kt
+++ src/main/kotlin/team/iwfsg/fsg/global/config/PropertiesScanConfig.kt
@@ -2,12 +2,14 @@ package team.iwfsg.fsg.global.config
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
+import team.iwfsg.fsg.global.redis.properties.RedisProperties
 import team.iwfsg.fsg.global.security.jwt.properties.SecurityProperties
 
 @Configuration
 @ConfigurationPropertiesScan(
         basePackageClasses = [
-            SecurityProperties::class
+            SecurityProperties::class,
+            RedisProperties::class
         ]
 )
 class PropertiesScanConfig {
---
+++ src/main/kotlin/team/iwfsg/fsg/global/redis/properties/RedisProperties.kt
@@ -0,0 +1,10 @@
+package team.iwfsg.fsg.global.redis.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+data class RedisProperties(
+    val host: String,
+    val port: Int
+)
