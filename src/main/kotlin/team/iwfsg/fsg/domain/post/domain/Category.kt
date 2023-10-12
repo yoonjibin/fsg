145175908f5b050c8402b364cfba1f09c3ee9921
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-10-12 11:41:23 +0900

    add category domain and entity

---
+++ src/main/kotlin/team/iwfsg/fsg/domain/post/domain/Category.kt
@@ -0,0 +1,7 @@
+package team.iwfsg.fsg.domain.post.domain
+
+data class Category(
+   val id: Long,
+   val content: String,
+   val postId: Long
+)
---
+++ src/main/kotlin/team/iwfsg/fsg/domain/post/persistence/entity/CategoryJpaEntity.kt
@@ -0,0 +1,18 @@
+package team.iwfsg.fsg.domain.post.persistence.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "post")
+data class CategoryJpaEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false)
+    val content: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "post_id")
+    val postId: PostJpaEntity
+)
