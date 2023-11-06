8a18d8f0b95bd660fca2f3def10a00f50b17634f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-06 15:09:02 +0900

    add errorCode

--- src/main/kotlin/team/iwfsg/fsg/global/error/ErrorCode.kt
+++ src/main/kotlin/team/iwfsg/fsg/global/error/ErrorCode.kt
@@ -5,5 +5,6 @@ enum class ErrorCode(val message: String, val status: Int) {
     INVALID_TOKEN("invalid token",401),
     FORBIDDEN("forbidden", 403),
     USER_NOT_FOUND("user not found", 404),
+    USER_ALREADY_EXIST("user already exist", 409),
     INTERNAL_SERVER_ERROR("internal server error", 500);
 }
