16a9d4b0206352c45ffdd187a9a6ffb153afe255
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-06 15:05:35 +0900

    update file directory

--- src/main/kotlin/team/iwfsg/fsg/domain/auth/data/dto/SignUpDto.kt
+++ src/main/kotlin/team/iwfsg/fsg/domain/auth/data/dto/SignUpDto.kt
@@ -1,4 +1,4 @@
-package team.iwfsg.fsg.domain.user.data.dto
+package team.iwfsg.fsg.domain.auth.data.dto
 
 data class SignUpDto (
     val name: String,
--- src/test/kotlin/team/iwfsg/fsg/domain/user/service/SignUpServiceTest.kt
+++ src/test/kotlin/team/iwfsg/fsg/domain/user/service/SignUpServiceTest.kt
@@ -2,21 +2,16 @@ package team.iwfsg.fsg.domain.user.service
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.ints.exactly
 import io.mockk.*
-import team.iwfsg.fsg.domain.auth.persistence.repository.RefreshTokenRepository
-import team.iwfsg.fsg.domain.user.data.dto.SignUpDto
+import team.iwfsg.fsg.domain.auth.data.dto.SignUpDto
 import team.iwfsg.fsg.domain.user.persistence.repository.UserRepository
-import team.iwfsg.fsg.global.security.jwt.JwtTokenProvider
 
 class SignUpServiceTest: BehaviorSpec({
     val name = "test1234"
     val email = "test@gmail.com"
     val password = "test1234"
 
-    val tokenProvider = mockk<JwtTokenProvider>()
     val userRepository = mockk<UserRepository>()
-    val repositoryRepository = mockk<RefreshTokenRepository>()
 
     Given("회원가입에 필요한 정보들이 주어졌을 때") {
         val request = SignUpDto(name, email, password)
